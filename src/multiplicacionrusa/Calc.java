1a00b7aed277c6a06e84d485b1ab83ba86d27834
PllGrd69 <mccalloluque@gmail.com>
2020-04-26 21:20:56 -0500

    version final

--- src/multiplicacionrusa/Calc.java
+++ src/multiplicacionrusa/Calc.java
@@ -2,34 +2,3 @@
 package multiplicacionrusa;
 
 
-public class Calc {
-    int a,b,c,d;
-    int acu=0;
-    public void condic(int a, int b){
-        
-        if(b%2==0){
-            acu=0;
-        }
-        else{
-            acu=a;
-            
-        }
-        while(b>=1){
-           c=a*2;
-           d=b/2;
-           a=c;
-           b=d;
-           
-            if(b%2==0){
-            
-            
-        }
-        else{
-            acu=acu+a;
-                     
-        }
-        
-    }
-        
-}
-}

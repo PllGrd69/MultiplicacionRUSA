6636a77e177cbba3d4a5125a4969766913999383
PllGrd69 <mccalloluque@gmail.com>
2020-04-26 20:45:28 -0500

    Version 1

--- src/multiplicacionrusa/MultiplicacionRusa.java
+++ src/multiplicacionrusa/MultiplicacionRusa.java
@@ -1,11 +1,20 @@
 
 package multiplicacionrusa;
 
+import java.util.Scanner;
 
 public class MultiplicacionRusa {
 
     public static void main(String[] args) {
+          int a,b;
+        int acu=0;
+        Scanner teclado = new Scanner (System.in); 
+        System.out.println("Ingrese el primer numero");
+        a = teclado.nextInt();
+        System.out.println("Ingrese el segundo numero");
+        b = teclado.nextInt();
+        
+      
+             }
     
-    
-}
 }

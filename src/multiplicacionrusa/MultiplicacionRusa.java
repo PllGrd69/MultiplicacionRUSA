c07254a92aaf298d3846355963885c24d02211a2
PllGrd69 <mccalloluque@gmail.com>
2020-04-26 21:23:35 -0500

    final

--- src/multiplicacionrusa/MultiplicacionRusa.java
+++ src/multiplicacionrusa/MultiplicacionRusa.java
@@ -13,39 +13,9 @@ public class MultiplicacionRusa {
         a = teclado.nextInt();
         System.out.println("Ingrese el segundo numero");
         b = teclado.nextInt();
-        
-        //Calc operacion = new Calc();
-        //operacion.condic(a,b);
-             }
-public class Calc {
-    int a,b,c,d;
-    int acu=0;
-    public void condic(int a, int b){
-        
-        if(b%2==0){
-            acu=0;
-        }
-        else{
-            acu=a;
-            
-        }
-        while(b>=1){
-           c=a*2;
-           d=b/2;
-           a=c;
-           b=d;
-           
-            if(b%2==0){
-            
-            
-        }
-        else{
-            acu=acu+a;
-                     
-        }
-        
-    }
-        System.out.println("Resultado: "+acu); 
+    
+      Calc operacion = new Calc();
+        operacion.condic(a,b);
 }
-}    
+    
 }
